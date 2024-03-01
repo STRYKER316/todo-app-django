b7984e12613b5e56d0f05aa142f50517edac9f90
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-01 23:39:56 +0530

    Add TaskAdmin class in admin.py to cutomize Task menu for admin panel

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
 from todo_app.models import Task
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('description', 'is_completed', 'created_at', 'updated_at')
+    list_filter = ('is_completed', 'created_at', 'updated_at')
+    search_fields = ('description',)
+
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
