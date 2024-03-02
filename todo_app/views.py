98c63bd56e0d825f4ce5bed438e9ccf9380a7987
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-02 13:10:57 +0530

    Modify form to accept submission request at corresponding view

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def add_task(request):
+    print(request.POST['task_form'])
+    return HttpResponse('Form submitted and Task added')
