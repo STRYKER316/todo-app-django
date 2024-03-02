0490bf757e1f549d37f76bf0b895fe989ed822cd
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-02 20:25:32 +0530

    Add a 'Mark as Undone' button functionality for completed tasks

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('add_task/', views.add_task, name='add_task'),
     path('complete_task/<int:task_id>/', views.complete_task, name='complete_task'),
+    path('revert_completed_task/<int:task_id>/', views.revert_completed_task, name='revert_completed_task'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect
-
 from .models import Task
 
 
-# Create your views here.
+# create a new task
 def add_task(request):
     task = request.POST['task_form']
     Task.objects.create(description=task)
@@ -11,9 +10,19 @@ def add_task(request):
     return redirect('home')
 
 
+# Mark a pending task as completed
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     task.is_completed = True
+    task.save()
+    
+    return redirect('home')
+
+
+# Mark a completed task as pending
+def revert_completed_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.is_completed = False
     task.save()
     
     return redirect('home')
